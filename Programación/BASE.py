d0d2e9f0bcdfe99941094ddb778b9e52239440a6
David Escutia de Haro <105667377+Dvix-dev@users.noreply.github.com>
2024-02-27 13:51:48 +0100

    Add files via upload

---
+++ Programación/BASE.py
@@ -0,0 +1,21 @@
+'''
+Descripción del programa: DESCRIPCION
+
+
+Version: 1.0
+Autor: David Escutia de Haro
+''' 
+#### Importar Librerias externas  #######
+
+
+##### Declaración de Variables #######
+
+
+
+###### Funciones y Procedimientos (Subprogramas) ######
+
+
+
+###### Programa Principal #############
+
+
