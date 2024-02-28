d0aae4aa9a7f229f3b4660b95cba5fc84665943f
David Escutia de Haro <105667377+Dvix-dev@users.noreply.github.com>
2024-02-28 17:50:16 +0000

    a

--- LenguajeDeMarcas/Puntuables/9 AF-JavaScript 3/main.js
+++ LenguajeDeMarcas/Puntuables/9 AF-JavaScript 3/main.js
@@ -11,25 +11,49 @@
 // oportunas a las 3 funciones anteriores, mostrando el resultado de las mismas.
 // El programa debe funcionar bien aunque el usuario no introduzca nada.
 
-var cadena1 = prompt("Introduzca una primera cadena:")
-var cadena2 = prompt("Introduzca una segunda cadena:")
 
-function concatenar() {
-    alert("Concatenar\n"  + cadena1 + cadena2)
-}
-
-function alreves() {
-    alert("Alreves\n" + cadena2 + cadena1)
-}
-
-function InsertarNumeros() {
-    for (let a = 0; a < cadena1.length; contador++) {
-     
-      
+// FUNCIONES
+function concatenar(cadena1,cadena2) {
+    if (cadena1.length === 0 || cadena2.length === 0) {
+        alert("Alguna de las cadenas está vacía")
     }
-    alert("Alreves" + "<br>" + cadena2 + cadena1)
+    else{
+        alert("Concatenar\n" + cadena1 + cadena2)
+    }
 }
 
-concatenar()
+function alreves(cadena) {
+    if (cadena.length >= 0) {
+        alert("Alreves\n" + cadena.split('').reverse().join(''))
+    }
+    else {
+        alert("La cadena se encuenrtra vacía")
+    }
+}
 
-alreves()
+function InsertarNumeros(cadena) {
+    // if (cadena.length >= 1) {
+        for (var i = 0; i < cadena.length; i++) {
+            cadenaFinal += cadena[i] + (i + 1);
+        }
+        alert("Cadena con numeros\n" + cadenaFinal);
+    // } else {
+    //     alert("La cadena está vacía")
+    // }
+    
+}
+
+
+// PROGRAMA PRINCIPAL
+    // FUNCION 1
+var variable1 = prompt("Introduzca una primera cadena para concatenar:");
+var variable2 = prompt("Introduzca una segunda cadena para concatenar:");
+concatenar(variable1,variable2);
+
+    // FUNCION 2
+var variable = prompt("Introduzca una cadena para poner al reves:");
+alreves(variable);
+
+    // FUNCION 3
+var variable = prompt("Introduzca una cadena para insertar numeros entre los caracteres:");
+InsertarNumeros(variable);
